78617e8b69afdb93cd068c277d1cbabe1416488f
Jonathan Coney <joncon@noc.ac.uk>
2025-01-10 11:10:47 +0000

    infill indentation

--- src/pyic/grid.py
+++ src/pyic/grid.py
@@ -184,60 +184,61 @@ class GRID:
         in1 = in1.cf.add_bounds(keys=["lon", "lat"])
         self.inset = in1  # Store the created inset dataset
 
+
 def infill(arr_in, n_iter=None, bathy=None):
-        """TODO: INTEGRATE WITH CLASS PROPERLY.
+    """TODO: INTEGRATE WITH CLASS PROPERLY.
 
-        Returns data with any NaNs replaced by iteratively taking the geometric
-        mean of surrounding points until all NaNs are removed or n_inter-ations
-        have been performed. Input data must be 2D and can include a
-        bathymetry array as to provide land barriers to the infilling.
+    Returns data with any NaNs replaced by iteratively taking the geometric
+    mean of surrounding points until all NaNs are removed or n_inter-ations
+    have been performed. Input data must be 2D and can include a
+    bathymetry array as to provide land barriers to the infilling.
 
-        Args:
-            arr_in          (ndarray): data array 2D
-            n_iter              (int): number of smoothing iterations
-            bathy           (ndarray): bathymetry array (land set to zero)
+    Args:
+        arr_in          (ndarray): data array 2D
+        n_iter              (int): number of smoothing iterations
+        bathy           (ndarray): bathymetry array (land set to zero)
 
-        Returns
-        -------
-            arr_mod         (ndarray): modified data array
-        """
-        # Check number of dims
-        if arr_in.ndim != 2:
-            raise ValueError("Array must have two dimensions")
+    Returns
+    -------
+        arr_mod         (ndarray): modified data array
+    """
+    # Check number of dims
+    if arr_in.ndim != 2:
+        raise ValueError("Array must have two dimensions")
 
-        # Intial setup to prime things for the averaging loop
-        if bathy is None:
-            bathy = np.ones_like(arr_in, dtype=float)
-        if n_iter is None:
-            n_iter = np.inf
+    # Intial setup to prime things for the averaging loop
+    if bathy is None:
+        bathy = np.ones_like(arr_in, dtype=float)
+    if n_iter is None:
+        n_iter = np.inf
+    ind = np.where(np.logical_and(np.isnan(arr_in), np.greater(bathy, 0.0)))
+    counter = 0
+    jpj, jpi = arr_in.shape
+    # Infill until all NaNs are removed or N interations completed
+    while np.sum(ind) > 0 and counter < n_iter:
+        # TODO: include a check to see if number of NaNs is decreasing
+
+        # Create indices of neighbouring points
+        ind_e = cp.deepcopy(ind)
+        ind_w = cp.deepcopy(ind)
+        ind_n = cp.deepcopy(ind)
+        ind_s = cp.deepcopy(ind)
+
+        ind_e[1][:] = np.minimum(ind_e[1][:] + 1, jpi - 1)
+        ind_w[1][:] = np.maximum(ind_w[1][:] - 1, 0)
+        ind_n[0][:] = np.minimum(ind_n[0][:] + 1, jpj - 1)
+        ind_s[0][:] = np.maximum(ind_s[0][:] - 1, 0)
+
+        # Replace NaNs
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)
+            arr_in[ind] = np.nanmean(
+                np.vstack((arr_in[ind_e], arr_in[ind_w], arr_in[ind_n], arr_in[ind_s])),
+                axis=0,
+            )
+
+        # Find new indices for next loop
         ind = np.where(np.logical_and(np.isnan(arr_in), np.greater(bathy, 0.0)))
-        counter = 0
-        jpj, jpi = arr_in.shape
-        # Infill until all NaNs are removed or N interations completed
-        while np.sum(ind) > 0 and counter < n_iter:
-            # TODO: include a check to see if number of NaNs is decreasing
+        counter += 1
 
-            # Create indices of neighbouring points
-            ind_e = cp.deepcopy(ind)
-            ind_w = cp.deepcopy(ind)
-            ind_n = cp.deepcopy(ind)
-            ind_s = cp.deepcopy(ind)
-
-            ind_e[1][:] = np.minimum(ind_e[1][:] + 1, jpi - 1)
-            ind_w[1][:] = np.maximum(ind_w[1][:] - 1, 0)
-            ind_n[0][:] = np.minimum(ind_n[0][:] + 1, jpj - 1)
-            ind_s[0][:] = np.maximum(ind_s[0][:] - 1, 0)
-
-            # Replace NaNs
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", category=RuntimeWarning)
-                arr_in[ind] = np.nanmean(
-                    np.vstack((arr_in[ind_e], arr_in[ind_w], arr_in[ind_n], arr_in[ind_s])),
-                    axis=0,
-                )
-
-            # Find new indices for next loop
-            ind = np.where(np.logical_and(np.isnan(arr_in), np.greater(bathy, 0.0)))
-            counter += 1
-
-        return arr_in
+    return arr_in
