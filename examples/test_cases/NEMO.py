217a141d938798747723b4834cbf2d4349894375
Jonathan Coney <joncon@noc.ac.uk>
2025-01-22 15:18:05 +0000

    formatting fix

--- examples/test_cases/NEMO.py
+++ examples/test_cases/NEMO.py
@@ -12,7 +12,9 @@ src_grid = grid.GRID(
     src_dom_path, ds_lon_name="nav_lon", ds_lat_name="nav_lat", ds_time_counter="time_counter"
 )
 
-src_data = grid.GRID(src_data_path, ds_lon_name="nav_lon", ds_lat_name="nav_lat", ds_time_counter="time_counter")
+src_data = grid.GRID(
+    src_data_path, ds_lon_name="nav_lon", ds_lat_name="nav_lat", ds_time_counter="time_counter"
+)
 
 dst_grid = grid.GRID(dst_dom_path, ds_lon_name="glamt", ds_lat_name="gphit", ds_time_counter="time_counter")
 
@@ -23,7 +25,7 @@ regrid1 = regrid.make_regridder(
     regrid_algorithm="bilinear",
     save_weights="~/NCMRWF/NEMO-regrid_weights_bilinear.nc",
     use_inset=False,
-    parallel=False
+    parallel=False,
 )
 
 regridded_T = regrid.regrid_data(src_data, regridder=regrid1)
