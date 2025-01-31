e458a94618389dc33bc1ba9283333333390db5c5
Jonathan Coney <joncon@noc.ac.uk>
2025-01-31 09:29:01 +0000

    barebones pyic_exe script

--- src/pyic/pyic_exe.py
+++ src/pyic/pyic_exe.py
@@ -1,5 +1,18 @@
+import sys
+
+from src.pyic.grid import GRID
+from src.pyic.regrid import make_regridder
+
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+
+
 def main():
     print("pyIC")
+    grid1 = GRID(file1)
+    grid2 = GRID(file2)
+    regridder = make_regridder(grid1, grid2, save_weights="weights.nc")
+    regridder.to_netcdf("regridded.nc")
 
 
 if __name__ == "__main__":
