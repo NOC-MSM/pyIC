511c8a47741cabbe5d0a89657f93b5bde470c0c0
Jonathan Coney <joncon@noc.ac.uk>
2025-03-07 09:55:37 +0000

    add vertical regridding test

--- tests/test_gridclass.py
+++ tests/test_gridclass.py
@@ -10,7 +10,7 @@ from .synthetic_data_gen import main as synmain
 synmain()
 
 
-@pytest.mark.parametrize(("filepath"), (["ds1.nc", "ds2.nc"]))
+@pytest.mark.parametrize(("filepath"), (["ds1.nc", "ds2.nc", "ds1_64.nc"]))
 def test_grid_ds(filepath):
     g1 = GRID(data_filename=filepath, ds_lon_name="lon", ds_lat_name="lat")
     ds = xr.open_dataset(filepath)
@@ -24,3 +24,16 @@ def test_synthetic_regrid():
     g2_regrid = regrid_data(g2, regridder=regridder)
     ds = xr.open_dataset("ds2_rgd.nc").isel(time_counter=0)
     np.testing.assert_array_equal(g2_regrid["temperature"].values, ds["temperature"].values)
+
+
+def test_synthetic_vertical_regrid():
+    g1 = GRID(data_filename="ds1_32.nc", ds_lon_name="lon", ds_lat_name="lat")
+    g2 = GRID(data_filename="ds2.nc", ds_lon_name="lon", ds_lat_name="lat")
+    regridder = make_regridder(g1, g2, check_superset=False)
+    g1_regrid = regrid_data(
+        g1,
+        regridder=regridder,
+        regrid_vertically=True,
+        vertical_kwargs={"levels": np.arange(0, 3600, 100), "vertical_coord": "depth", "method": "linear"},
+    )
+    np.testing.assert_array_equal(g1_regrid["temperature"].shape, g2.common_grid["temperature"].shape)
